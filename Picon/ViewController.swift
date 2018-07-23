3cac05c0a270a7f27db7c8d6e74e2b8452204ce2
Frans <fransvandenabeele@imac-van-frans.telenet.be>
2018-07-23 22:59:09 +0200

    Initial Commit

---
+++ Picon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picon
+//
+//  Created by Frans on 23/07/18.
+//  Copyright © 2018 Frans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
