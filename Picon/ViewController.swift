69358b3ede91ec060ae26f2c449185e0d914ec09
Frans Vandenabeele <frans@imac-van-frans.telenet.be>
2018-11-13 19:51:20 +0100

    Eerste checkin

--- Picon/ViewController.swift
+++ Picon/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var piconNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        piconNameLabel.text = textField.text
     }
-
-
+    @IBAction func Reset(_ sender: UIButton) {
+        piconNameLabel.text = "PICON"
+    }
+    
 }
 
